36ac101dbb0ad1613ce2727ea78e7fc4e755d99e
m2web <m2web@yahoo.com>
2019-07-06 14:39:33 -0400

    Corrected test file

--- countriesArray.test.js
+++ countriesArray.test.js
@@ -3,3 +3,7 @@ const countries = require('./countriesArray');
 test('First Link to be https://prayercast.com/afghanistan.html', () => {
   expect(countries[0]).toBe("https://prayercast.com/afghanistan.html");
 });
+
+test('Last Link to be https://prayercast.com/zimbabwe.html', () => {
+  expect(countries[countries.length-1]).toBe("https://prayercast.com/zimbabwe.html");
+});
